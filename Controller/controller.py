579cf0728e4145ffe088f63c271e5e510ee175b7
EgrNvk <egrnvk@gmail.com>
2025-12-17 11:35:10 +0200

    Added logger in controller

--- Controller/controller.py
+++ Controller/controller.py
@@ -3,22 +3,29 @@ import time
 
 from DTO.DTO import FieldBonusDTO
 from View import view
-
+from Services.Logger_Service import logger
 
 class FarmController:
     def __init__(self, model):
         self.model = model
+        logger.info("FarmController створено")
 
     def buy_fertilizer(self, fert_name):
+        logger.debug(f"Спроба купити добрево: {fert_name}")
         ok = self.model.buy_fertilizer(fert_name)
         if not ok:
+            logger.warning(f"Не вдалося купити добрево: {fert_name}")
             view.show_warning("Недостатньо грошей або помилка покупки.")
+        else:
+            logger.info(f"Добрево куплено: {fert_name}")
         view.update_all()
 
     def field_clicked(self, field_index):
+        logger.debug(f"Клік по полю {field_index+1}")
         field = self.model.fields[field_index]
 
         if not field.unlocked:
+            logger.info(f"Поле {field_index+1} спроба покупкі заблакованого полю")
             base_price = self.model.field_prices[field_index]
 
             super_fert = next(
@@ -36,9 +43,11 @@ class FarmController:
             )
 
             if choice is None:
+                logger.info(f"Покупку поля {field_index+1} скасовано")
                 return
 
             if choice == "bonus":
+                logger.warning(f"Недостатньо коштів для покупки з бонусом поля {field_index+1}")
                 if self.model.balance < bonus_price:
                     view.show_warning(
                         f"Недостатньо грошей, щоб купити поле {field_index + 1} з бонусом. "
@@ -46,6 +55,7 @@ class FarmController:
                     )
                     return
 
+                logger.info(f"Поле {field_index+1} куплено з бонусом")
                 self.model.balance -= bonus_price
                 field.unlocked = True
                 if super_fert:
@@ -59,12 +69,14 @@ class FarmController:
 
             if choice == "normal":
                 if self.model.balance < base_price:
+                    logger.warning(f"Недостатньо коштів для покупки з бонусом поля {field_index+1}")
                     view.show_warning(
                         f"Недостатньо грошей, щоб відкрити поле {field_index + 1}. "
                         f"Потрібно {base_price}₴."
                     )
                     return
 
+                logger.info(f"Поле {field_index+1} куплено без бонуса")
                 self.model.balance -= base_price
                 field.unlocked = True
                 field.bonus = None
@@ -73,13 +85,16 @@ class FarmController:
                 return
 
         if field.state == "empty":
+
             plant_name = view.plant_var.get()
             fert_name = view.fert_var.get()
             if fert_name == "Без добрива":
                 fert_name = None
+            logger.info(f"Посадка на полі {field_index+1}: рослина {plant_name}, добрево {fert_name}")
 
             ok = self.model.plant_on_field(field_index, plant_name, fert_name)
             if not ok:
+                logger.warning(f"Не вдалося посадити рослину {plant_name} на полі {field_index+1}")
                 view.show_warning("Поле зайняте або немає добрива.")
                 return
 
@@ -91,10 +106,12 @@ class FarmController:
             view.show_warning("Культура ще росте!")
 
         elif field.state == "ready":
+            logger.info(f"Збір уражаю з поля {field_index+1}")
             ok = self.model.harvest(field_index)
             if ok:
                 view.update_field_bg(field_index, 0)
             else:
+                logger.error(f"Помилка збору уражая з поля {field_index+1}")
                 view.show_warning("Помилка збору!")
             view.update_all()
 
@@ -125,11 +142,14 @@ class FarmController:
         view.update_all()
 
     def sell_crop(self, plant_name):
+        logger.debug(f"Спроба продажу: {plant_name}")
         gained = self.model.sell(plant_name)
 
         if gained == 0:
+            logger.warning(f"Продаж не вдався: {plant_name}")
             view.show_warning("У амбарі немає такого урожаю.")
         else:
+            logger.info(f"Продано {plant_name}, отримано {gained}₴")
             view.update_sell_info(f"+{gained}₴ отримано!")
 
         view.update_all()
