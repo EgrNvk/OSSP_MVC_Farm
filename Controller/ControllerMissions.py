d0645605b42161ad551afc5c416021ab437141b2
EgrNvk <egrnvk@gmail.com>
2025-12-18 13:41:40 +0200

    Create ControllerMissions in controller

---
+++ Controller/ControllerMissions.py
@@ -0,0 +1,29 @@
+class ControllerMissions:
+    def __init__(self, model):
+        self.model = model  # FarmModel
+
+    def after_field_unlocked(self):
+        m = self.model.missions
+        m.unlocked_fields = sum(
+            1 for f in self.model.fields if getattr(f, "unlocked", False)
+        )
+        m.check_all()
+
+    def after_plant(self, fertilizer_used: bool):
+        if not fertilizer_used:
+            m = self.model.missions
+            m.organic_plantings += 1
+            m.check_all()
+
+    def after_harvest(self):
+        m = self.model.missions
+        m.wheat_count = int(self.model.ambar.get("Пшениця", 0))
+        m.check_all()
+
+    def after_balance_change(self):
+        m = self.model.missions
+        m.money = int(self.model.balance)
+        m.check_all()
+
+    def after_tick(self):
+        self.model.missions.current_session_time += 1
