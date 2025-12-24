012d96a95296b633d3b4cf0a44568845ededb190
EgrNvk <egrnvk@gmail.com>
2025-12-24 10:59:07 +0200

    Added init tests in test_FarmModel

--- tests/test_FarmModel.py
+++ tests/test_FarmModel.py
@@ -2,6 +2,15 @@ import unittest
 from app.Model.FarmModel import FarmModel
 
 class TestFarmModel(unittest.TestCase):
-    def test_initial_balance_exists(self):
+    def test_init_balance(self):
         model = FarmModel()
-        self.assertIsNotNone(model.balance)
+        self.assertEqual(model.balance, 50)
+
+    def test_init_fields_count(self):
+        model = FarmModel()
+        self.assertEqual(len(model.fields), model.max_fields)
+
+    def test_init_first_fields_unlocked(self):
+        model = FarmModel()
+        for i in range(4):
+            self.assertTrue(model.fields[i].unlocked)
